6a9651b959a7c59864c88ad4e641c9ad0cec860e
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-10-08 01:46:47 +0600

    added navbar

--- src/Router/routes.jsx
+++ src/Router/routes.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
 import MainLayout from "../Layout/MainLayout";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Login from "../Pages/Login";
+import Orders from "../Pages/Orders";
+import Register from "../Pages/Register";
 
 const routes = createBrowserRouter([
   {
@@ -10,6 +15,26 @@ const routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
